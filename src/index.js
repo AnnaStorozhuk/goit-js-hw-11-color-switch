d1b40cfd98afb107fd1a3fc1f8f8f70ca2660a2f
Anna Storozhuk <annastorozhuk17@gmail.com>
2021-07-09 11:44:14 +0300

    исправление ошибки

--- src/index.js
+++ src/index.js
@@ -1,28 +1,50 @@
-const refs = {
-    startBtn: document.querySelector('button[data-action="start"'),
-    stopBtn: document.querySelector('button[data-action="stop"'),
-  },
-  colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ],
-  randomIntegerFromInterval = (max) => {
-    return Math.floor(Math.random() * (max + 1));
-  },
-  setRandomColor = () => {
-    const _color = colors[randomIntegerFromInterval(colors.length - 1)]
-    console.log(_color)
-    document.body.style.backgroundColor = _color
-  }
+
+colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let isActive = false;
 
 let interval = undefined
 
-refs.startBtn.addEventListener('click', e => interval = interval ?
-  interval :
-  setInterval(() => setRandomColor(), 1000))
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  bodyRef: document.querySelector('body'),
+}
+  
 
-refs.stopBtn.addEventListener('click', e => clearInterval(interval))
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  if (isActive) {
+    return
+  }
+  interval = setInterval(onChangingColor, 1000);
+  isActive = true;
+  refs.startBtn.disabled = true;
+}
+  
+function stopChangeColor() {
+ 
+  clearInterval(interval);
+  isActive = false;
+  refs.startBtn.disabled = false;
+}
+
+function onChangingColor() {
+ let randomIndexOfElement = randomIntegerFromInterval(0, colors.length - 1);
+  let colorOfBody = colors[randomIndexOfElement];
+  refs.bodyRef.style.backgroundColor = colorOfBody;
+    
+  }
